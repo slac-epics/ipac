ad6db08e6b24ec59dc3d1880b01c92b2875b921a
Sheng Peng <pengs@slac.stanford.edu>
2009-05-06 01:41:37 +0000

    Add back xipIoParse

--- drvIpac/drvXy9660.c
+++ drvIpac/drvXy9660.c
@@ -42,12 +42,14 @@ Copyright (c) 2007 UChicago Argonne LLC.
 #include <string.h>
 #include <ctype.h>
 
+#include "xipIo.h"
 #include <devLib.h>
 #include <epicsThread.h>
 #include <epicsExport.h>
 #include <epicsExit.h>
 #include <iocsh.h>
 
+LOCAL char *strdupn( const char *ct, size_t n );
 #include "drvIpac.h"
 
 #define epicsAssertAuthor "Andrew Johnson <anj@aps.anl.gov>"
@@ -379,7 +381,77 @@ static int irqCmd (
     }
 }
 
+int xipIoParse( char *str, xipIo_t *ptr, char flag )
+{
+  char *name;
+  char *end;
 
+  if( str == NULL || ptr == NULL )
+    return 1;
+
+  while( !isalnum(*str) )
+  {
+    if( *str++ == '\0')
+      return 1;
+  }
+
+  name = str;
+
+  str = strpbrk(str, " ");
+  if( str == NULL || *str == '\0' )
+    return 1;
+
+  ptr->name = strdupn(name, str - name);
+  if( ptr->name == NULL )
+    return 1;
+
+  if( flag == 'A' )
+  {
+    end = strchr(str,'C');
+    if( end )
+    {
+      str = end + 1;
+      sscanf(str,"%d", &ptr->channel);
+    }
+    else
+      return 1;
+  }
+  else if( flag == 'B' )
+  {
+    end = strchr(str,'P');
+    if( end )
+    {
+      str = end + 1;
+      sscanf(str,"%d", &ptr->port);
+      end = strchr(str,'B');
+      if( end )
+      {
+        str = end + 1;
+        sscanf(str,"%d", &ptr->bit);
+      }
+      else
+        return 1;
+    }
+    else
+      return 1;
+  }
+  return(0);
+}
+
+LOCAL char *strdupn( const char *ct, size_t n )
+{
+  char *duplicate;
+
+  duplicate = (char *)malloc(n+1);
+  if( !duplicate )
+    return NULL;
+
+  memcpy(duplicate, ct, n);
+  duplicate[n] = '\0';
+
+  return duplicate;
+}
+                         
 /* IPAC Carrier Table */
 
 static ipac_carrier_t xy9660 = {
