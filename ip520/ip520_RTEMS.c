9c4cb9f6aa14310b66a79611f57903e748a0cbf2
Jeremy Lorelli <lorelli@slac.stanford.edu>
2025-04-22 16:29:53 -0700

    Fix build errors on RTEMS 6

--- ip520/ip520_RTEMS.c
+++ ip520/ip520_RTEMS.c
@@ -34,7 +34,10 @@ Implentation Notes:
 #include <termios.h>
 #include <errno.h>
 #include <iocsh.h>
+#include <rtems.h>
+#if __RTEMS_MAJOR__ <= 4
 #include <rtems/system.h>
+#endif
 #include <rtems/io.h>
 #include <rtems/error.h>
 #include <rtems/termiostypes.h>
